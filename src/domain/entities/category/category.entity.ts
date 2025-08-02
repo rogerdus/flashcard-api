631491da3bcb4006ed13f65ab4305327c0210e4b
rogerdus4 <rogerdus@gmail.com>
2025-08-01 19:03:44 -0600

    feat: undertanding ports, repositories and entities

---
+++ src/domain/entities/category/category.entity.ts
@@ -0,0 +1,6 @@
+export interface CategoryEntity {
+    id: string;
+    name: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ src/domain/entities/flashcard/flashcard.entity.ts
@@ -0,0 +1,7 @@
+export interface FlashcardEntity {
+    id: string;
+    question: string;
+    answer: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
